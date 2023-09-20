0de7023ef3775503ba95ed384e55277f815c8c89
Idedav <xdavide.correa@gmail.com>
2023-09-20 14:03:52 +0200

    [ADD] randomizer

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const numA = randomizer(1,100);
+const numB = randomizer(1,100);
+const numC = randomizer(1,100);
+const numD = randomizer(1,100);
+const numE = randomizer(1,100);
+
+
+
+
+
+
+
+// -----------FUNCTIONS-------------
+function randomizer(min,max){
+    return Math.floor((Math.random() * max ) + min );
+}
